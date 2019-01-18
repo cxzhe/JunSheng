dcedcde6c43dd48aedbe64407b6d7efba5389568
magerate <magerate1129@gmail.com>
2019-01-18 16:09:45 +0800

    implement setting

---
+++ JZExample/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JZExample.Model;
+
+namespace JZExample
+{
+    public class AppContext
+    {
+        public BatchInfo[] BatchsToPrint { get; set; }
+
+        public static readonly AppContext Instance = new AppContext();
+
+        private AppContext()
+        {
+        }
+    }
+}
---
+++ JZExample/SettingForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace JZExample
+{
+    public partial class SettingForm : Form
+    {
+        public SettingForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            textBox1.Text = Settings.Default.X30Ip;
+            textBox2.Text = Settings.Default.QRField;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            Settings.Default.X30Ip = textBox1.Text;
+            Settings.Default.QRField = textBox2.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
